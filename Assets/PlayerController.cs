de351658d2b44a544543bf342bb02f4729448c45
Headdrop2000 <38054968+Headdrop2000@users.noreply.github.com>
2018-05-01 18:46:27 -0400

    Game Press Space to hit red boxes into the cup

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public Rigidbody2D Rigid;
+    public float JumpForce;
+    public float GravityMultipler;
+    float AddGravity;
+
+    // Use this for initialization
+    void Start () {
+
+
+        {
+            AddGravity += GravityMultipler;
+        }
+           
+        
+
+
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Rigid.AddForce(force: Vector2.right * JumpForce, mode: ForceMode2D.Impulse);
+
+        }
+
+        {
+            Rigid.AddForce((Vector3.left * AddGravity), ForceMode2D.Impulse);
+        }
+    }
+
+
+}
+    
---
+++ Assets/SpawnController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnController : MonoBehaviour {
+    public GameObject wallPrefab;
+    
+	// Use this for initialization
+	void Start () {
+        StartCoroutine(SpawnWall());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+
+IEnumerator SpawnWall()
+    {
+
+        Instantiate(wallPrefab, transform.position, Quaternion.identity);
+      
+
+
+        yield return new WaitForSeconds(1);
+
+        StartCoroutine(SpawnWall());
+
+
+
+    }
+
+
+
+}
